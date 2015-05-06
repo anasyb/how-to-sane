c6c07ee8fd0740c82ddba627af47e0a1d22da3c1
anasyb <anasyb@gmail.com>
2015-05-05 19:35:48 -0700

    illustrate transitioning on auth
    This is more of a brainstorming than a cooked PR. I think it would be cool to illustrate transitioning based on whether the user is authenticated or not. At the moment, this home route mixes login with whatever, but it would be cool to actually transition the user to login if not logged in, and transition him/her to say his/her dashboard at `/user/:id`.
    
    If one would want to go in more details some configs could be added to config/environment.js:
    ```
          authenticationRoute:         'login',
          routeAfterAuthentication:    'application',
          routeIfAlreadyAuthenticated: 'application',
    ```

--- client/app/pods/index/route.js
+++ client/app/pods/index/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	beforeModel: function(transition, queryParams){
+		if (this.get('session.isAuthenticated')){
+			this.transitionTo('user', this.get('session.user.owner') );
+		} else {
+			this.transitionTo('login');
+		}
+		this._super(transition, queryParams);
+	},
 });
